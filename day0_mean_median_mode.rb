f7c792fbe8268e40e588a21681deba2e3b0663c6
toowan <mail@toowan.me>
2016-12-28 14:32:55 -0800

    Day 0: Mean, Median, Mode

--- day0_mean_median_mode.rb
+++ day0_mean_median_mode.rb
@@ -2,30 +2,33 @@
 n = gets.to_i
 array = gets.split(" ").map(&:to_i)
 
-# def mean(array, n)  
-#   # Take the sum of all the elements in the array and divide by n
-#   ((array.inject(0){|sum, x| sum + x}) / n.to_f)
-# end
+def mean(array, n)  
+  # Take the sum of all the elements in the array and divide by n
+  ((array.inject(0){|sum, x| sum + x}) / n.to_f)
+end
 
-# def median(array, n) 
-#   # sort the array
-#   sorted_array = array.sort 
-#   # get the average of the two middle numbers
-#   (sorted_array[(n-1)/2] + sorted_array[n/2]) / 2.0
-# end
+def median(array, n) 
+  # sort the array  
+  sorted_array = array.sort 
+  # get the average of the two middle numbers
+  (sorted_array[(n-1)/2] + sorted_array[n/2]) / 2.0
+end
 
 def mode(array, n)
+  sorted_array = array.sort 
   number_ocurrences = Hash.new(0)
 
   # count the number of occurences of each value
-  array.each do |number|
+  sorted_array.each do |number|
     number_ocurrences[number] += 1
   end
 
-  # Returns the key of the min key-value pair based on the value.  
-  number_ocurrences.min_by{|k, v| v}[0]
+  # Returns the key of the key-value pair based on the max value.  
+  number_ocurrences.max_by{|k, v| v}[0]
 
 end
 
-
+puts mean(array, n)
+puts median(array, n)
 puts mode(array, n)
+
