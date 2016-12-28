4eddd31d3b060319b849da07dec9e60c50c56602
toowan <mail@toowan.me>
2016-12-28 14:32:07 -0800

    Day 0: Mean, Median, Mode

---
+++ day0_mean_median_mode.rb
@@ -0,0 +1,31 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = gets.to_i
+array = gets.split(" ").map(&:to_i)
+
+# def mean(array, n)  
+#   # Take the sum of all the elements in the array and divide by n
+#   ((array.inject(0){|sum, x| sum + x}) / n.to_f)
+# end
+
+# def median(array, n) 
+#   # sort the array
+#   sorted_array = array.sort 
+#   # get the average of the two middle numbers
+#   (sorted_array[(n-1)/2] + sorted_array[n/2]) / 2.0
+# end
+
+def mode(array, n)
+  number_ocurrences = Hash.new(0)
+
+  # count the number of occurences of each value
+  array.each do |number|
+    number_ocurrences[number] += 1
+  end
+
+  # Returns the key of the min key-value pair based on the value.  
+  number_ocurrences.min_by{|k, v| v}[0]
+
+end
+
+
+puts mode(array, n)
